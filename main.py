72f8bbb9a8f2d4d88dd42e59cfebbd8ef16f9bb9
cyai <83634399+cyai@users.noreply.github.com>
2024-09-19 23:35:11 +0530

    feat: use multi messages

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 openai_api_base = "http://localhost:8000/v1"
 
+audios = {
+    1: "https://cdn.discordapp.com/attachments/1285851100615938106/1286386611805753414/cr1.mp3?ex=66edb864&is=66ec66e4&hm=129a5f7bee0e7708159344cd220ae35bfdbf318d00e44c95b5596ae83ba64bbc&",
+    2: "https://cdn.discordapp.com/attachments/1285851100615938106/1286386649424596992/lm1.mp3?ex=66edb86d&is=66ec66ed&hm=7fc6fedb4113dc9fbbdae6b52d99562afa9260ab7f307052c6ae3f888e255f04&",
+}
+
+messages = []
 client = OpenAI(
     # defaults to os.environ.get("OPENAI_API_KEY")
     api_key=openai_api_key,
@@ -26,30 +32,6 @@ client = OpenAI(
 
 models = client.models.list()
 model = models.data[0].id
-
-# Any format supported by librosa is supported
-audio_url = AudioAsset("winning_call").url
-
-# Use audio url in the payload
-chat_completion_from_url = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "What's in this audio?"},
-                {
-                    "type": "audio_url",
-                    "audio_url": {"url": audio_url},
-                },
-            ],
-        }
-    ],
-    model=model,
-    max_tokens=64,
-)
-
-result = chat_completion_from_url.choices[0].message.content
-print(f"Chat completion output:{result}")
 
 
 # Use base64 encoded audio in the payload
@@ -63,26 +45,49 @@ def encode_audio_base64_from_url(audio_url: str) -> str:
     return result
 
 
-audio_base64 = encode_audio_base64_from_url(audio_url=audio_url)
-chat_completion_from_base64 = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "What's in this audio?"},
-                {
-                    "type": "audio_url",
-                    "audio_url": {
-                        # Any format supported by librosa is supported
-                        "url": f"data:audio/ogg;base64,{audio_base64}"
+for i in range(1, 3):
+    audio_base64 = encode_audio_base64_from_url(audio_url=audios[i])
+    if i == 1:
+        print(f"Audio 1 base64: {audio_base64}")
+        chat_completion_from_base64 = client.chat.completions.create(
+            messages={
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "What's in this audio?"},
+                    {
+                        "type": "audio_url",
+                        "audio_url": {
+                            # Any format supported by librosa is supported
+                            "url": f"data:audio/ogg;base64,{audio_base64}"
+                        },
                     },
-                },
-            ],
-        }
-    ],
-    model=model,
-    max_tokens=64,
-)
+                ],
+            },
+            model=model,
+            max_tokens=64,
+        )
+    else:
+        print(f"Audio 2 base64: {audio_base64}")
+        chat_completion_from_base64 = client.chat.completions.create(
+            messages={
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "How is the person in the previous audio related to the one in this audio?",
+                    },
+                    {
+                        "type": "audio_url",
+                        "audio_url": {
+                            # Any format supported by librosa is supported
+                            "url": f"data:audio/ogg;base64,{audio_base64}"
+                        },
+                    },
+                ],
+            },
+            model=model,
+            max_tokens=64,
+        )
 
-result = chat_completion_from_base64.choices[0].message.content
-print(f"Chat completion output:{result}")
+    result = chat_completion_from_base64.choices[0].message.content
+    print(f"Chat completion output:{result}")
